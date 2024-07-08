76448071077d290c995f18cbf11756700a31aa6b
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2024-07-08 21:23:30 +0800

    Add: Sidebar

---
+++ components/mobileSideBar.tsx
@@ -0,0 +1,21 @@
+import { Button } from "./ui/button"; 
+import { Menu } from "lucide-react";
+import { Sheet, SheetTrigger, SheetContent } from "./ui/sheet";
+import Sidebar from "./sideBar";
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default MobileSidebar;

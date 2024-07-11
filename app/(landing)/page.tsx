1ea379c9c0534629559426429502dbf66d45b30c
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2024-07-12 00:12:23 +0800

    Add: Landing Page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,22 +1,13 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { LandingNavbar } from "@/components/landingNavbar";
+import { LandingHero } from "@/components/landingHero";
+import { LandingContent } from "@/components/landingContent";
 
 const LandingPage = () => {
   return (
-    <div>
-        Landing Page
-        <div>
-          <Link href="/sign-in">
-            <Button>
-              Login
-            </Button>
-          </Link>
-          <Link href="/sign-up">
-            <Button>
-              Register
-            </Button>
-          </Link>
-        </div>
+    <div className="h-full">
+       <LandingNavbar />
+       <LandingHero />
+       <LandingContent />
     </div>
   );
 }
